73f0f2e5cf74d40cadc0b0f156febaefb91974bb
AntonioCardenas <antonioalx66@gmail.com>
2022-12-01 05:54:47 -0600

    .

--- src/components/common/LangChooser.tsx
+++ src/components/common/LangChooser.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import button from "react-bootstrap/esm/button";
 import { useTranslation } from "react-i18next";
 import { DE, ES, US, FR } from "country-flag-icons/react/3x2";
 
