5119f9edb42a7ac73ad22f7812aabd212818879c
AntonioCardenas <antonioalx66@gmail.com>
2022-12-20 13:33:39 -0600

    Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import { HelmetProvider } from 'react-helmet-async';
 
 import { CookiesProvider } from "react-cookie";
 import "./i18n";
@@ -14,60 +13,23 @@ import '../src/assets/css/globals.css';
 import '../src/assets/css/range-slider.css';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
+import DynamicHeader from "./components/DynamicHeader/DynamicHeader";
 
 const clientOracle = () =>
   new ApolloClient({
     uri: "https://api.thegraph.com/subgraphs/name/jdestephen/affogato-sg",
     cache: new InMemoryCache(),
   });
-function getFaviconEl() {
-    return document.getElementById("favicon");
-}
+
 
 const App = () => {
 
-
-
   const contracts = useContracts();
   const apolloClient = clientOracle();
-  const favicon = getFaviconEl(); // Accessing favicon element
-
-  const [cooperative, setCompany] = useState("");
-  useEffect(() => {
-    const location = window.location.host;
-
-    if (location.match("commovel") !== null) {
-      setCompany('Commovel');
-      // @ts-ignore
-      favicon.href = "assets/favicon/commovel.ico";
-    }
-    if (location.match("copranil") !== null) {
-      setCompany('Copranil');
-      // @ts-ignore
-      favicon.href = "assets/favicon/copranil.ico";
-    }
-    if (location.match("comsa") !== null) {
-      setCompany('Comsa');
-      // @ts-ignore
-      favicon.href = "assets/favicon/comsa.ico";
-    }
-    if (location.match("proexo") !== null) {
-      setCompany('Proexo');
-      // @ts-ignore
-      favicon.href = "assets/favicon/proexo.ico";
-    } else {
-      setCompany('Plataforma de trazabilidad');
-      // @ts-ignore
-      favicon.href = "assets/favicon/favicon.ico";
-    }
-  })
 
   return (
       <>
-      <HelmetProvider>
-          <title>  {cooperative}</title>
-          <link rel="icon" type="image/png" href="assets/comsa.png" sizes="16x16" />
-      </HelmetProvider>
+ <DynamicHeader/>
     <AuthProvider>
       <contractsContext.Provider value={contracts}>
         <CookiesProvider>
---
+++ src/components/DynamicHeader/DynamicHeader.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {HelmetProvider} from "react-helmet-async";
+function getFaviconEl() {
+    return document.getElementById("favicon");
+}
+
+const DynamicHeader = () => {
+    const favicon = getFaviconEl();
+    console.log(favicon);
+    const location = window.location.host;
+
+    if (location.match("commovel") !== null) {
+        // @ts-ignore
+        favicon.href = "../../assets/favicon/commovel.ico";
+        return (<>
+                <HelmetProvider>
+                    <title>  Commovel </title>
+                    <link rel="icon" type="image/png" href="../../assets/favicon/commovel.ico" sizes="16x16" />
+                </HelmetProvider>
+            </>
+        );
+    }
+    if (location.match("copranil") !== null) {
+        // @ts-ignore
+        favicon.href = "../../assets/favicon/copranil.ico";
+        return (<>
+                <HelmetProvider>
+                    <title> Copranil </title>
+                    <link rel="icon" type="image/png" href="../../assets/favicon/copranil.ico" sizes="16x16" />
+                </HelmetProvider>
+            </>
+        );    }
+    if (location.match("comsa") !== null) {
+        // @ts-ignore
+        favicon.href = "../../assets/favicon/comsa.ico";
+        return (<>
+                <HelmetProvider>
+                    <title>  Comsa </title>
+                    <link rel="icon" type="image/png" href="../../assets/favicon/comsa.ico" sizes="16x16" />
+                </HelmetProvider>
+            </>
+        );    }
+    if (location.match("proexo") !== null) {
+        // @ts-ignore
+        favicon.href = "../../assets/favicon/proexo.ico";
+        return (<>
+                <HelmetProvider>
+                    <title>  Proexo </title>
+                    <link rel="icon" type="image/png" href="../../assets/favicon/proexo.ico" sizes="16x16" />
+                </HelmetProvider>
+            </>
+        );
+    } else {
+        // @ts-ignore
+        favicon.href = "../../assets/favicon/default.ico";
+        return (<>
+                <HelmetProvider>
+                    <title>  Plataforma de trazabilidad </title>
+                    <link rel="icon" type="image/png" href="../../assets/favicon/default.ico" sizes="16x16" />
+                </HelmetProvider>
+            </>
+        );
+    }
+
+};
+export default DynamicHeader;
