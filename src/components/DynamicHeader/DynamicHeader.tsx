cf940c5b60e06629065d181adb0a2ce94b228378
ɐntonio cardenas <antonioalx66@gmail.com>
2023-02-22 22:43:31 -0600

    Update DynamicHeader.tsx

--- src/components/DynamicHeader/DynamicHeader.tsx
+++ src/components/DynamicHeader/DynamicHeader.tsx
@@ -10,7 +10,7 @@ const DynamicHeader = () => {
 
     if (location.match("commovel") !== null) {
         // @ts-ignore
-        favicon.href = "../../assets/favicon/commovel.ico";
+        favicon.href = "https://affogato.co/src/assets/favicon/commovel.ico";
         return (<>
                 <HelmetProvider>
                     <title> Commovel </title>
@@ -21,41 +21,41 @@ const DynamicHeader = () => {
     }
     if (location.match("copranil") !== null) {
         // @ts-ignore
-        favicon.href = "../../assets/favicon/copranil.ico";
+        favicon.href = "https://affogato.co/src/assets/favicon/copranil.ico";
         return (<>
                 <HelmetProvider>
                     <title> Copranil </title>
-                    <link rel="icon" type="image/png" href="../../assets/favicon/copranil.ico" sizes="16x16" />
+                    <link rel="icon" type="image/png" href="https://affogato.co/src/assets/favicon/copranil.ico" sizes="16x16" />
                 </HelmetProvider>
             </>
         );    }
     if (location.match("comsa") !== null) {
         // @ts-ignore
-        favicon.href = "../../assets/favicon/comsa.ico";
+        favicon.href = "https://affogato.co/src/assets/favicon/proexo.ico";
         return (<>
                 <HelmetProvider>
                     <title> Comsa </title>
-                    <link rel="icon" type="image/png" href="../../assets/favicon/comsa.ico" sizes="16x16" />
+                    <link rel="icon" type="image/png" href="https://affogato.co/src/assets/favicon/comsa.ico" sizes="16x16" />
                 </HelmetProvider>
             </>
         );    }
     if (location.match("proexo") !== null) {
         // @ts-ignore
-        favicon.href = "../../assets/favicon/proexo.ico";
+        favicon.href = "https://affogato.co/src/assets/favicon/proexo.ico";
         return (<>
                 <HelmetProvider>
                     <title> Proexo </title>
-                    <link rel="icon" type="image/png" href="../../assets/favicon/proexo.ico" sizes="16x16" />
+                    <link rel="icon" type="image/png" href="https://affogato.co/src/assets/favicon/proexo.ico" sizes="16x16" />
                 </HelmetProvider>
             </>
         );
     } else {
         // @ts-ignore
-        favicon.href = "../../assets/favicon/default.ico";
+        favicon.href = "https://affogato.co/src/assets/favicon/favicon/default.ico";
         return (<>
                 <HelmetProvider>
                     <title> Plataforma de trazabilidad </title>
-                    <link rel="icon" type="image/png" href="../../assets/favicon/default.ico" sizes="16x16" />
+                    <link rel="icon" type="image/png" href="https://affogato.co/src/assets/favicon/favicon/default.ico" sizes="16x16" />
                 </HelmetProvider>
             </>
         );
