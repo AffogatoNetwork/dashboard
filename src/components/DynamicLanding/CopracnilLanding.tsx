95c4a653595d0c965705c5fa05ff0333cb34de43
ɐntonio cardenas <antonioalx66@gmail.com>
2023-05-19 12:10:15 -0600

    fix copracnil

--- src/components/DynamicLanding/CopracnilLanding.tsx
+++ src/components/DynamicLanding/CopracnilLanding.tsx
@@ -217,7 +217,7 @@ export const CopracnilLanding = () => {
                      className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-                            COPRACNIL Cooperativas Nuevas Ideas Limitada
+                            COPRACNIL Cooperativa de Productores Cafetaleros Nuevas Ideas Limitada
                         </h2>
 
                     </div>
