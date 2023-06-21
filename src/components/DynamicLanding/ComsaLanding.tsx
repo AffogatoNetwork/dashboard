0eb717fa591891a3e1cc391a51348dc92e221e31
Antonio Cardenas <antonioalx66@gmail.com>
2023-06-21 15:02:58 -0600

    Fix:21JUN:1502

--- src/components/DynamicLanding/ComsaLanding.tsx
+++ src/components/DynamicLanding/ComsaLanding.tsx
@@ -21,8 +21,7 @@ export const ComsaLanding = () => {
                     style={{ backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/affogato-fde9c.appspot.com/o/Comsa%2F1.jpeg?alt=media&token=01904488-a6bd-4c1e-9dec-10b3b9abd6bf")` }}>
                     <div className="hero-overlay bg-opacity-60 bg-black rounded-t-2xl"></div>
                     <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-md">
-                            <h1 className="max-w-2xl mb-4 text-6xl font-black md:text-6xl xl:text-7xl text-white">
+<div className="max-w-lg">                            <h1 className="max-w-2xl mb-4 text-6xl font-black md:text-6xl xl:text-7xl text-white">
                                 Plataforma de trazabilidad de COMSA
                             </h1>
                             <p className="max-w-2xl mb-6 text-3xl font-light lg:mb-8 md:text-lg lg:text-xl xl:text-4xl text-white">
