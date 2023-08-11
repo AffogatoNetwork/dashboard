ede62eb41f1465135387b05f33848a7b2beaa2d7
Antonio Cardenas <antonioalx66@gmail.com>
2023-08-11 15:47:31 -0600

    landing

--- src/components/CoffeeBatch/Landing.tsx
+++ src/components/CoffeeBatch/Landing.tsx
@@ -1,55 +1,55 @@
 import React from "react";
-import {DefaultLanding} from "../DynamicLanding/DefaultLanding";
-import {CommovelLanding} from "../DynamicLanding/CommovelLanding";
-import {CopracnilLanding} from "../DynamicLanding/CopracnilLanding";
-import {ComsaLanding} from "../DynamicLanding/ComsaLanding";
-import {ProexoLanding} from "../DynamicLanding/ProexoLanding";
+import { DefaultLanding } from "../DynamicLanding/DefaultLanding";
+import { CommovelLanding } from "../DynamicLanding/CommovelLanding";
+import { CopracnilLanding } from "../DynamicLanding/CopracnilLanding";
+import { ComsaLanding } from "../DynamicLanding/ComsaLanding";
+import { ProexoLanding } from "../DynamicLanding/ProexoLanding";
 import { CafepsaLanding } from "../DynamicLanding/CafepsaLanding";
 
 const Landing = () => {
     const location = window.location.host;
 
     if (location.match("commovel") !== null) {
-            return (<>
-<div className="rounded">
-    <CommovelLanding></CommovelLanding>
-</div>
-                </>
-            );
-        }
-        if (location.match("copracnil") !== null) {
-            return (<>
-                    <CopracnilLanding></CopracnilLanding>
-                </>
-            );
-        }
-        if (location.match("comsa") !== null) {
-            return (<>
-                    <ComsaLanding></ComsaLanding>
-                </>
-            );
-        }
-        if (location.match("proexo") !== null) {
-            return (<>
-                    <ProexoLanding></ProexoLanding>
-                </>
-            );
-        }
-        if (location.match("cafepsa") !== null) {
-            return (<>
-                    <CafepsaLanding></CafepsaLanding>
-                </>
-            );
-        }
+        return (<>
+            <div className="rounded">
+                <CommovelLanding></CommovelLanding>
+            </div>
+        </>
+        );
+    }
+    if (location.match("copracnil") !== null) {
+        return (<>
+            <CopracnilLanding></CopracnilLanding>
+        </>
+        );
+    }
+    if (location.match("comsa") !== null) {
+        return (<>
+            <ComsaLanding></ComsaLanding>
+        </>
+        );
+    }
+    if (location.match("proexo") !== null) {
+        return (<>
+            <ProexoLanding></ProexoLanding>
+        </>
+        );
+    }
+    if (location.match("cafepsa") !== null) {
+        return (<>
+            <DefaultLanding></DefaultLanding>
+        </>
+        );
+    }
 
 
 
-            return (
-                <>
-                    <CafepsaLanding/>
-                    </>
-            );
+    return (
+        <>
+            <CafepsaLanding />
+        </>
+    );
 
-    };
+};
 
 export default Landing;
