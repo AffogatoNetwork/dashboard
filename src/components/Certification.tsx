890a71e3f662ab5f90f1df01fbda4b01e5a7863d
ᐸʸᵉᵒᵘᵈᵉᵛ/ᐳ <antonioalx66@gmail.com>
2024-12-19 16:08:40 -0600

    fix:fairtrade

--- src/components/Certification.tsx
+++ src/components/Certification.tsx
@@ -213,6 +213,15 @@ const Certification = () => {
           <div className="relative bg-white overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
             <img
               className="object-cover w-full h-56 md:h-64 xl:h-80 "
+              src={require('../assets/certificaciones/2_Fair Trade.png')}
+              alt=""
+            />
+            <p className="mb-4 text-lg font-bold text-center">Fair Trade</p>
+          </div>
+
+          <div className="relative bg-white overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+            <img
+              className="object-cover w-full h-56 md:h-64 xl:h-80 "
               src={require('../assets/certificaciones/2_Fair Trade.jpeg')}
               alt=""
             />
